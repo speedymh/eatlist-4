0bd40838b0127f72e350ca9a643f9c2500547369
speedymh <matthias_hoppe@outlook.de>
2016-11-05 20:51:30 +0100

    added arrow functionality
    not totally done yet

--- function.js
+++ function.js
@@ -2,5 +2,10 @@ $(function () {
 	$('.register').on('click', function () {
 		$('.input').toggleClass('show');
 		$('.i-register').toggleClass('show');
+		$('.i-login').toggleClass('show');
+	});
+	$('.login').on('click', function () {
+		$('.input').toggleClass('show');
+		$('.i-login').toggleClass('show');
 	});
 });
