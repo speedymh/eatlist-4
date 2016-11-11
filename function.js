c79f16b4f29421c936ae951813797e538d6297a1
speedymh <matthias_hoppe@outlook.de>
2016-11-11 14:58:36 +0100

    complete functionality of buttons

--- function.js
+++ function.js
@@ -4,16 +4,19 @@ $(function () {
 			//Send it
 			$('.i-login').removeClass('show');
 			$('.i-register').addClass('show');
+			$('.input-login').removeClass('show');
+			$('.input-register').addClass('show');
 		} else {
-			$('.input-login').addClass('show');
+			$('.input-register').addClass('show');
 			$('.i-register').addClass('show');
 			$('.i-login').removeClass('show');
 		}
 	});
 	$('.login').on('click', function () {
-		if($('.input-login').hasClass('show')) {
+		if($('.input-register').hasClass('show')) {
 			//Send it
-			$('.input-login').removeClass('show');
+			$('.input-register').removeClass('show');
+			$('.input-login').addClass('show');
 			$('.i-login').addClass('show');
 			$('.i-register').removeClass('show');
 		} else {
