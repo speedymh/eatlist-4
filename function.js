de52188aa9f48fdfc8d3f1094510e906ff8de87e
speedymh <matthias_hoppe@outlook.de>
2016-11-11 08:52:39 +0100

    button toggle not ready yet

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 $(function () {
 	$('.register').on('click', function () {
 		$('.input').toggleClass('show');
-		$('.i-register').toggleClass('show');
-		$('.i-login').toggleClass('show');
+		$('.i-register').addClass('show');
+		$('.i-login').removeClass('show');
 	});
 	$('.login').on('click', function () {
 		$('.input').toggleClass('show');
-		$('.i-login').toggleClass('show');
+		$('.i-login').addClass('show');
+		$('.i-register').removeClass('show');
 	});
 });
