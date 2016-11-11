d68438e92b214d3de91cde69687604b5c5116843
speedymh <matthias_hoppe@outlook.de>
2016-11-11 09:08:03 +0100

    js toggle button arrow
    input-register und input-login hinzugefügt

--- function.js
+++ function.js
@@ -1,12 +1,25 @@
 $(function () {
 	$('.register').on('click', function () {
-		$('.input').toggleClass('show');
-		$('.i-register').addClass('show');
-		$('.i-login').removeClass('show');
+		if($('.input-login').hasClass('show')) {
+			//Send it
+			$('.i-login').removeClass('show');
+			$('.i-register').addClass('show');
+		} else {
+			$('.input-login').addClass('show');
+			$('.i-register').addClass('show');
+			$('.i-login').removeClass('show');
+		}
 	});
 	$('.login').on('click', function () {
-		$('.input').toggleClass('show');
-		$('.i-login').addClass('show');
-		$('.i-register').removeClass('show');
+		if($('.input-login').hasClass('show')) {
+			//Send it
+			$('.input-login').removeClass('show');
+			$('.i-login').addClass('show');
+			$('.i-register').removeClass('show');
+		} else {
+			$('.input').toggleClass('show');
+			$('.i-register').removeClass('show');
+			$('.i-login').addClass('show');
+		}
 	});
 });
